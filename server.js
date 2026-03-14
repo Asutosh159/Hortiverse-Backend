a6019f89c3a84a43a41219fb265c87f4eeed518a
1asutosh1 <sahooasutosh690@gmail.com>
2026-03-14 19:51:56 +0530

    backendapichange commit

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // ==========================================
